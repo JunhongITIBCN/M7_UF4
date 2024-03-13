a2f996c667f53f434b7368aa300e9aab2ae446d0
JunhongITICBCN <2023_junhong.zhu@iticbcn.cat>
2024-03-13 18:44:36 +0100

    Complentado enlaces

--- TIC_BCN_JHZ/centre/urls.py
+++ TIC_BCN_JHZ/centre/urls.py
@@ -4,5 +4,8 @@ from .import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('student/',views.student, name='alumn'),  
-    path('teacher/<int:pk>', views.teacher, name='prof'),
+    path('teacher/', views.teacher, name='prof'),
+    path('teacher/teachers/<str:pk>', views.teachers, name ='teacher'),
+    path('student/students/<str:pk>', views.students, name ='student')
+
 ]
--- TIC_BCN_JHZ/centre/views.py
+++ TIC_BCN_JHZ/centre/views.py
@@ -98,3 +98,102 @@ def teacher(request):
     template = loader.get_template('prof.html')
     return HttpResponse(template.render({'teachers': teachers}, request))
 
+
+def teachers(request, pk):
+    teachers = [
+        {
+            'id': '1',
+            'nom': 'Roger',
+            'cognom': 'Sobrino',
+            'edat': 39,
+            'rol': 'teacher',
+            'curs': ['DAM2B', 'DAW2A']
+        },
+        {
+            'id': '2',
+            'nom': 'Josep Oriol',
+            'cognom': 'Roca',
+            'edat': 25,
+            'rol': 'teacher',
+            'curs': ['DAM2B', 'DAW2A', 'DAW1A']
+        },
+        {
+            'id': '3',
+            'nom': 'Juanma',
+            'cognom': 'Sanchez',
+            'edat': 24,
+            'rol': 'teacher',
+            'curs': ['DAM2B', 'DAW2A']
+        }
+    ]
+    teachers_Obj = None
+    for i in teachers :
+        if i['id'] == pk:
+            teachers_Obj = i
+            return render(request, 'teacher.html', {'teacher': teachers_Obj})
+
+
+
+def students(request, pk):
+    students = [
+        {
+            'id': '1',
+            'nom': 'Jun',
+            'cognom1': 'Zhu',
+            'cognom2': 'Zhang',
+            'correu': '2023_junhong.zhu@iticbcn.cat',
+            'curs': 'DAW2A',
+            'moduls_matriculats': ['Mòdul 9', 'Mòdul 7' , 'Mòdul 6' , 'Mòdul 5']
+        },
+        {
+            'id': '2',
+            'nom': 'Joel',
+            'cognom1': 'Ghanem',
+            'cognom2': 'Gomez',
+            'correu': '2023_joel.ghanem@iticbcn.cat',
+            'curs': 'DAW2A',
+            'moduls_matriculats':  ['Mòdul 9', 'Mòdul 7' , 'Mòdul 6']
+        },
+        {
+            'id': '3',
+            'nom': 'Anxo',
+            'cognom1': 'Aragundi',
+            'cognom2': 'Mesias',
+            'correu': '2023_anxo.aragundi@iticbcn.cat',
+            'curs': 'DAW2A',
+            'moduls_matriculats':  ['Mòdul 9', 'Mòdul 7' , 'Mòdul 6']
+        },
+        {
+            'id': '4',
+            'nom': 'Eric',
+            'cognom1': 'Sanchez',
+            'cognom2': 'Vasquez',
+            'correu': '2023_eric.sanchez@iticbcn.cat',
+            'curs': 'DAW2A',
+            'moduls_matriculats':  ['Mòdul 9', 'Mòdul 7' , 'Mòdul 6']
+        },
+        {
+            'id': '5',
+            'nom': 'Dinar',
+            'cognom1': 'Khazimullin',
+            'cognom2': 'notiene',
+            'correu': '2023_dinar.khazimullin@iticbcn.cat',
+            'curs': 'DAW2A',
+            'moduls_matriculats':  ['Mòdul 9', 'Mòdul 7' , 'Mòdul 6']
+        },
+        {
+            'id': '6',
+            'nom': 'Carlos',
+            'cognom1': 'Zambrano',
+            'cognom2': 'aray',
+            'correu': '2023_carlos.zambrano@iticbcn.cat',
+            'curs': 'DAW2A',
+            'moduls_matriculats':  ['Mòdul 9', 'Mòdul 7' , 'Mòdul 6']
+        }
+    ]
+    students_Obj = None
+    for i in students :
+        if i['id'] == pk:
+            students_Obj = i
+    return render(request, 'student.html', {'student': students_Obj})
+
